4a6be39e9721264f94223643f18ca9693af59047
Kevin Saliou <kevin@b2deal.com>
2011-09-14 23:09:01 +0200

    remove clearfix which made batch actions to look funny

--- data/generator/sfDoctrineModule/mpReality/template/templates/_list.php
+++ data/generator/sfDoctrineModule/mpReality/template/templates/_list.php
@@ -6,9 +6,9 @@
   <div class="paginate_top">
        <div class="sf_admin_actions f_left">
                         [?php include_partial('<?php echo $this->getModuleName() ?>/list_batch_actions', array('helper' => $helper)) ?]
-                       
+
                     </div>
-    
+
     [?php include_partial('<?php echo $this->getModuleName() ?>/pagination', array('pager' => $pager)) ?]
   </div>
  [?php endif; ?]
@@ -35,19 +35,19 @@
               [?php endif; ?]
               <div class="f_left list_results">
               [?php echo format_number_choice('[0] no result|[1] 1 result|(1,+Inf] %1% results', array('%1%' => $pager->getNbResults()), $pager->getNbResults(), 'sf_admin') ?]
-            
+
               [?php include_partial('<?php echo $this->getModuleName() ?>/pagination_list_select', array( 'pager' => $pager)) ?]
                 [?php if ($pager->haveToPaginate()): ?]
               [?php echo __(' (%%page%%/%%nb_pages%%)', array('%%page%%' => $pager->getPage(), '%%nb_pages%%' => $pager->getLastPage()), 'sf_admin') ?]
               [?php endif; ?]
               </div>
-                <div class="clearfix"></div>
+                <!--<div class="clearfix"></div>-->
                [?php include_partial('<?php echo $this->getModuleName() ?>/list_actions', array('helper' => $helper)) ?]
             </th>
           </tr>
         </tfoot>
-        
-	
+
+
 	<tbody class='{toggle_url: "[?php echo url_for($helper->getRouteArrayForAction('toggleBoolean', '<?php echo $this->getModuleName() ?>')) ?]"}'>
           [?php foreach ($pager->getResults() as $i => $<?php echo $this->getSingularName() ?>): $odd = fmod(++$i, 2) ? 'odd' : 'even' ?]
           <tr class="sf_admin_row [?php echo $odd ?] {pk: [?php echo $<?php echo $this->getSingularName() ?>->getPrimaryKey() ?]}">
