9f235c22cded909343671f7366815a8a506455e0
Kevin Saliou <kevin@b2deal.com>
2011-09-15 18:40:43 +0200

    comment out 'use_javascript' calls in plugin causing extra empty .js file to be loaded in the head

--- data/generator/sfDoctrineModule/mpReality/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/mpReality/template/templates/_assets.php
@@ -17,13 +17,13 @@
 [?php use_javascript('<?php echo $this->params['js'] ?>', 'first') ?]
 <?php elseif(!isset($this->params['js'])): ?>
 [?php use_javascript('<?php echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery-1.4.4.min.js' ?>', 'first') ?]
-[?php use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery-ui.custom.min.js' ?>', 'first') ?]
+[?php // use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery-ui.custom.min.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery-ui-1.8.7.custom.min.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.metadata.js' ?>', 'first') ?]
-[?php use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.ui.widget.js' ?>', 'first') ?]
+[?php // use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.ui.widget.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/ui.selectmenu.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/customInput.jquery.js' ?>', 'first') ?]
-[?php use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/ui.checkbox.js' ?>', 'first') ?]
+[?php // use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/ui.checkbox.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.blockUI.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.tipsy.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/functions.js' ?>', 'first') ?]
@@ -31,12 +31,11 @@
 <?php endif; ?>
 
 [?php // additionnal javascript (filament group)
-  use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/ui.core.js' ?>');
-  use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.bind.js' ?>');
-  use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/ui.checkbox.js' ?>');
-  use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.selectbox-0.5_style_2.js' ?>');
-  use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/custom_jquery.js' ?>');
-  use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.tooltip.js' ?>');
-  use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.dimensions.js' ?>');
-  
+// use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/ui.core.js' ?>');
+// use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.bind.js' ?>');
+// use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/ui.checkbox.js' ?>');
+// use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.selectbox-0.5_style_2.js' ?>');
+// use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/custom_jquery.js' ?>');
+// use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.tooltip.js' ?>');
+// use_javascript('<?php //echo sfConfig::get('app_mp_reality_admin_plugin', '/mpRealityAdminPlugin').'/js/jquery.dimensions.js' ?>');
 ?]
